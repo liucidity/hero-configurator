238350cd5d77ed855538c97d1663cf082aadeb38
Travis <travisliu708@gmail.com>
2023-03-02 16:07:06 -0800

    initial commit to set up deepai

--- src/pages/deepAI.js
+++ src/pages/deepAI.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
+
 const DeepAI = () => {
+  const deepai = require('deepai')
+  deepai.setApiKey('b4e79a79-4603-40a6-b825-9737b7ce11de')
+
+    (async function () {
+      var resp = await deepai.callStandardApi("text2img", {
+        text: "YOUR_TEXT_HERE",
+      });
+      console.warn(resp);
+    })()
   return (
     <div>
 
